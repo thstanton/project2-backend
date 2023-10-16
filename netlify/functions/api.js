4f14a2bdcfeaaeca3a0db6102c30da0085d61028
thstanton <thstanton@proton.me>
2023-10-16 15:03:05 +0100

    Serverless 13

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -30,17 +30,17 @@ router.get('/gigs/agency/:agencyId', gigs.agency)
 router.get('/gigs/stats/agencies', gigs.agencystats)
 router.get('/gigs/stats/agencies/:agencyId', gigs.agencystats)
 router.get('/gigs/venue/:venueId', gigs.venue)
-router.get('/gigs/:id', gigs.getOne)
 router.post('/gigs/new', gigs.new)
 router.put('/gigs/update/:id', gigs.update)
 router.delete('/gigs/delete/:id', gigs.delete)
+router.get('/gigs/:id', gigs.getOne)
 
 // ? Agencies
 router.get('/agencies', agencies.getAll)
-router.get('/agencies/:id', agencies.getOne)
 router.post('/agencies/new', agencies.new)
 router.put('/agencies/update/:id', agencies.update)
 router.delete('/agencies/delete/:id', agencies.delete)
+router.get('/agencies/:id', agencies.getOne)
 
 // ? Venues
 router.get('/venues', venues.getAll)
